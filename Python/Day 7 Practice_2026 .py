0cc36c6e83374b3282f50b32f40d3b056c4e7d6b
Sanju <codify.sanju@gmail.com>
2026-03-03 23:45:57 +0530

    Add files via upload

---
+++ Python/Day 7 Practice_2026 .py
@@ -0,0 +1,16 @@
+"""#Day 7
+
+##Q. Create a list with numbers between 1 to 10 and the values should be squared
+"""
+
+##[1,4,9,16...]
+
+lst = []
+
+for i in range (1,11):
+  lst.append(i**2)
+
+print(lst)
+
+"""##Q. Create a list with numbers between 1 to 100 and the value should contain only even numbers?"""
+
