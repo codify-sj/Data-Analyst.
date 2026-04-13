d35bc7e9c91762c0443f02ce802d3d076f24e3b0
Sanju <codify.sanju@gmail.com>
2026-04-13 23:56:35 +0530

    Day 10 Practice_2026.py

--- Python/Day 10 Practice_2026.py
+++ Python/Day 10 Practice_2026.py
@@ -1,16 +1,11 @@
-
-"""#Day 10
-
-##Numpy
-"""
+"""#Day 10"""
 
 !pip install streamlit
 
+!git clone "https://github.com/HarshvardhanSingh-13/Datasets"
 
 import pandas as pd
 
-df = pd.read_csv('/content/Datasets/Gaana/songs.csv')
+df = pd.read_csv('/content/Datasets/Gaana/songs.csv')   #(df is dataframe)
 
 df
-
-df.info()
