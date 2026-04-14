f7952a04e0d18937095084dffb4b25ceb6a599e2
Sanju <codify.sanju@gmail.com>
2026-04-15 00:03:21 +0530

    Day 10 Practice_2026.py

--- Python/Day 10 Practice_2026.py
+++ Python/Day 10 Practice_2026.py
@@ -4,8 +4,59 @@
 
 !git clone "https://github.com/HarshvardhanSingh-13/Datasets"
 
+"""#Data Exploration
+
+## 1.1)Importing libary
+"""
+
 import pandas as pd
+
+"""## 1.2)Loading libary"""
 
 df = pd.read_csv('/content/Datasets/Gaana/songs.csv')   #(df is dataframe)
 
 df
+
+df.info()
+
+df.describe()
+
+df.isnull().sum()
+
+df['singer']
+
+df['singer'] == 'jagjit singh'
+
+df[df['language'] == 'Urdu']
+
+"""## Q1) Find songs of singer name Lata Mangeshkar
+
+## Q2) FInd songs of singer Lata Mangeshkar which have language Hindi
+"""
+
+
+
+df['singer'] == 'Lata Mangeshkar'
+
+df[(df['singer'] == 'Lata Mangeshkar') & (df['language']=='Hindi')]
+
+df[df['singer'] == 'Jagjit Singh']
+
+df[(df['singer'] == 'Jagjit Singh') & (df['language']=='Urdu')]
+
+
+
+
+
+df['language'].unique()
+
+df['language'].nunique()
+
+df['singer'].unique()
+
+df['singer'].nunique()
+
+len(df['singer'].unique())
+
+len(df[df['language'] == 'Urdu'])
+
