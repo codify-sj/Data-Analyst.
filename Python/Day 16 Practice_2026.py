516c45e4fdc2b7da6b00d9d1dbb862c95f881e8b
Sanju <codify.sanju@gmail.com>
2026-05-11 23:19:19 +0530

    Day 16 Practice_2026.py

--- Python/Day 16 Practice_2026.py
+++ Python/Day 16 Practice_2026.py
@@ -83,6 +83,170 @@ df = pd.read_excel('/content/Datasets/Flight Prices/flight_price.xlsx')
 
 df
 
+'''
+KPI's
+
+1) Average Ticket Price by Airline
+2) Average Ticket Price by Route (Source → Destination)
+3) Average Price by Number of Stops
+4) Flight Volume by Airline
+5) Flight Volume by Source Airport
+6) Monthly Price Trend
+7) Average Departure Hour by Airline
+'''
+
+"""#### 1.2) basic data exploration"""
+
+df.info()
+
+df.describe()
+
+"""#### 1.3) Checking Null Values"""
+
+df.isnull().sum()
+
+"""#### 1.4) Removing Null Values"""
+
+df.dropna(subset = ['Route','Total_Stops'],inplace=True)
+
+df.isnull().sum()
+
+df.head(2)
+
+"""# 2) Column Wise Cleaning
+
+#### 2.1) Airlines
+"""
+
+df['Airline'].unique()
+
+df['Airline'].value_counts()
+
+"""#### 2.2) Date Of Journey"""
+
+df.head(2)
+
+df['Date_of_Journey'].value_counts()
+
+df.head(2)
+
+df['DATE'] = df['Date_of_Journey'].str.strip().str.split('/').str[0]
+
+df['MONTH'] = df['Date_of_Journey'].str.strip().str.split('/').str[1]
+df['YEAR'] = df['Date_of_Journey'].str.strip().str.split('/').str[2]
+
+df.head(2)
+
+df['DATE'] = df['DATE'].astype(int)
+df['MONTH'] = df['MONTH'].astype(int)
+df['YEAR'] = df['YEAR'].astype(int)
+
+df.info()
+
+date = []
+for i in df['Date_of_Journey']:
+  date.append(i.split('/')[0])
+
+df['DATE'] = date
+
+df.head(2)
+
+df['Destination'].unique()
+
+del df['Route']
+
+df
+
+df.head(2)
+
+df['Dep_HRS'] = df['Dep_Time'].str.split(':').str[0]
+df['Dep_MIN'] = df['Dep_Time'].str.split(':').str[1]
+
+df.head(2)
+
+del df['Dep_Time']
+del df['Date_of_Journey']
+
+df.head(2)
+
+df['Arrival_HRS'] = df['Arrival_Time'].str.split(' ').str[0].str.split(':').str[0]
+df['Arrival_Mins'] = df['Arrival_Time'].str.split(' ').str[0].str.split(':').str[1]
+
+df.head(2)
+
+del df['Arrival_Time']
+
+df.head(2)
+
+df['Duartion_hrs'] = df['Duration'].str.split(' ').str[0].replace('h','').str[0]
+
+df['Duartion_min'] = df['Duration'].str.replace('m','').str.strip(' ').str.split(' ').str[1]
+
+df.head(2)
+
+df['Duartion_min'].fillna(0,inplace=True)
+
+df['Duartion_min'].unique()
+
+df['Duration_hrs'] = df['Duartion_hrs'].astype(int)
+
+df.head(2)
+
+del df['duration_hrs']
+
+df.head(2)
+
+df.info()
+
+df.head(2)
+
+df['Duration_hrs'] = df['Duration_hrs']*60
+
+df.head(2)
+
+df['Duartion_min'] = df['Duartion_min'].astype(int)
+
+df['Total_Duration'] = df['Duration_hrs'] + df['Duartion_min']
+
+df.head(2)
+
+df.head(2)
+
+del df['Duartion_min']
+del df['Duration_hrs']
+
+df.head(2)
+
+df['Total_Stops'].unique()
+
+df['Additional_Info'].unique()
+
+del df['Additional_Info']
+
+df.head(2)
+
+df.to_csv('Cleaned_Data.csv')
+
+'''
+KPI's
+
+1) Average Ticket Price by Airline
+2) Average Ticket Price by Route (Source → Destination)
+3) Average Price by Number of Stops
+4) Flight Volume by Airline
+5) Flight Volume by Source Airport
+6) Monthly Price Trend
+7) Average Departure Hour by Airline
+
+'''
+
+df.head(2)
+
+df.groupby('Airline')['Price'].mean().sort_values(ascending=False)
+
+# 2) Average Ticket Price by Route (Source → Destination)
+
+df.groupby(['Source','Destination'])['Price'].mean().sort_values(ascending=False)
 
 
 
